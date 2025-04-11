ee37ddc1efb2bda75b548df17e0ed69caae7f441
Eric Xiao <ericxiao0000@gmail.com>
2025-04-10 17:12:34 -0700

    dynamic grid size

--- app/grid.tsx
+++ app/grid.tsx
@@ -2,42 +2,22 @@ import Image from "next/image";
 import metadata from "@/public/metadata.json";
 
 export default function Grid() {
-  const thing = metadata.map((collectionData, i) => {
-    return Array.from(Array(collectionData.images.length).keys()).map((j) => (
-      <Image
-        key={i + " " + j}
-        src={`/photography/${collectionData.name}/${j}.jpeg`}
-        alt="photo"
-        className="w-full aspect-[3/2]" // FIX vertical images are stretched
-        width={1}
-        height={1}
-      />
-    ));
-  });
-  console.log(thing);
   return (
-    <div className="grid grid-cols-4">
-      {thing}
-      {/* {Array.from(Array(9).keys()).map((i) => ( */}
-      {/*   <Image */}
-      {/*     key={i} */}
-      {/*     src={`/photography/steveston/${i}.jpeg`} */}
-      {/*     alt="photo" */}
-      {/*     className="w-full aspect-[3/2]" // FIX vertical images are stretched */}
-      {/*     width={1} */}
-      {/*     height={1} */}
-      {/*   /> */}
-      {/* ))} */}
-      {/* {Array.from(Array(9).keys()).map((i) => ( */}
-      {/*   <Image */}
-      {/*     key={i} */}
-      {/*     src={`/photography/steveston/${i}.jpeg`} */}
-      {/*     alt="photo" */}
-      {/*     className="w-full aspect-[3/2]" // FIX vertical images are stretched */}
-      {/*     width={1} */}
-      {/*     height={1} */}
-      {/*   /> */}
-      {/* ))} */}
+    <div className="grid 2xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
+      {metadata.map((collectionData, i) => {
+        return Array.from(Array(collectionData.images.length).keys()).map(
+          (j) => (
+            <Image
+              key={i + " " + j}
+              src={`/photography/${collectionData.name}/${j}.jpeg`}
+              alt="photo"
+              className="w-full aspect-[3/2]" // FIX vertical images are stretched
+              width={1}
+              height={1}
+            />
+          ),
+        );
+      })}
     </div>
   );
 }
